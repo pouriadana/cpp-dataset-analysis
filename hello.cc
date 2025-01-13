ca5913824437568d0942c63f45f92f599937b85a
Pouria Dana <pouriadana73@gmail.com>
2025-01-13 11:40:32 +0330

    read headers into an array

--- hello.cc
+++ hello.cc
@@ -4,7 +4,28 @@
 #include <string>
 #include <vector>
 
+/* Symbolic constants */
+#define MAX_COLS 50
+
 int main()
 {
-    
+    std::string columns_arr[MAX_COLS];
+    std::string s;
+    std::fstream file("Imdb Movie Dataset.csv");
+    file >> s;
+    std::string word = "";
+    int col_sub = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == ',') {
+            columns_arr[col_sub++] = word;
+            word = "";
+            ++i;
+        }
+        word += s[i];
+    }
+    columns_arr[col_sub++] = word;
+    for (int i = 0; i < MAX_COLS; ++i) {
+        std::cout << columns_arr[i] << '\n';
+    }
+    std::cout << std::endl;
 }

2fec59db4cfff126e780b9c0f0d6831ee8e5afcc
Pouria Dana <pouriadana73@gmail.com>
2025-01-14 09:31:04 +0330

    read data & print the header

--- hello.cc
+++ hello.cc
@@ -10,21 +10,10 @@
 
 int main()
 {
-    // std::string columns_arr[MAX_COLS];
-    int line_no = 1;
-    std::vector<std::vector<std::string>> data;
-    std::string s;
     std::fstream in("high_popularity_spotify_data.csv");
-    while (std::getline(in, s)) {
-        std::vector<std::string> temp_v;
-        std::stringstream line(s);
-        while (std::getline(line, s, ','))
-            temp_v.push_back(s);
-        data.push_back(temp_v);
-    }
-
-    for (const auto item : data[0]) {
+    std::vector<std::vector<std::string>> my_data{read_csv(in)};
+    for (const auto item : my_data[0]) {
             std::cout << item << '\n';
-        }
+    }
     std::cout << std::endl;
 }

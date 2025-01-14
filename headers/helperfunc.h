32709f70f7767886ce1d60ab2b5e73a63db5b2f6
Pouria Dana <pouriadana73@gmail.com>
2025-01-14 09:30:50 +0330

    complete read_csv()

--- headers/helperfunc.h
+++ headers/helperfunc.h
@@ -1,4 +1,24 @@
 #ifndef HLP_FUNC
 #define HLP_FUNC
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+std::vector<std::vector<std::string>> read_csv(std::fstream& in)
+// read a comma seperated line into a 2d vector with type std::string
+{
+    std::string s;
+    std::vector<std::vector<std::string>> out;
+    while (std::getline(in, s)) {
+        std::vector<std::string> temp_v;
+        std::stringstream line(s);
+        while (std::getline(line, s, ','))
+            temp_v.push_back(s);
+        out.push_back(temp_v);
+    }
+    return out;
+} 
 
 #endif
